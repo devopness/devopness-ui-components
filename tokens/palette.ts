7f9e86571974fa7e75120ae7206164130e12d21b
Yuri Jean Fabris <yuri.fabris@gmail.com>
2021-11-17 21:21:30 -0300

    feat: add design tokens

---
+++ tokens/palette.ts
@@ -0,0 +1,16 @@
+export const palette = {
+  primary: "#786efd",
+
+  transparent: "transparent",
+
+  gray50: "#9198A5",
+  gray40: "#2E374E",
+  gray35: "#4a526a",
+  gray30: "#828795",
+  gray20: "#E3E8F9",
+  gray10: "#72798f",
+  gray5: "#42495A",
+  gray: "#ABB4C5",
+
+  white: "#fff",
+};
